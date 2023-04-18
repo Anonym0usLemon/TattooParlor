ed18692f9251fb7420e86f6b87597f4331984ebf
Anonym0usLemon <76603698+Anonym0usLemon@users.noreply.github.com>
2023-04-18 16:11:49 -0500

    added /about, /footer, and started /FAQ section

--- App.js
+++ App.js
@@ -21,8 +21,8 @@ export default function App() {
         <Route path="tattooartists" element={<TattooArtists />}/>
       </Route> 
       </Routes>
+      <Footer/>
     </BrowserRouter>
-    <Footer/>
     </>
   );
 }
